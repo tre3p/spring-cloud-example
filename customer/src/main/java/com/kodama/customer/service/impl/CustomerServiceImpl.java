177e05129c2ba838f59edf902856be56e4978d05
noname <whereareyo@yahoo.com>
2022-04-06 22:57:42 +0300

    eureka-server added

--- customer/src/main/java/com/kodama/customer/service/impl/CustomerServiceImpl.java
+++ customer/src/main/java/com/kodama/customer/service/impl/CustomerServiceImpl.java
@@ -30,7 +30,7 @@ public class CustomerServiceImpl implements CustomerService {
         customerRepository.saveAndFlush(customer);
 
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
