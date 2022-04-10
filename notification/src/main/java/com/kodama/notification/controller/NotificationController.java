d6b970910f346fd16589988f058f7cfcefb8cc4d
noname <whereareyo@yahoo.com>
2022-04-10 17:41:01 +0300

    zipkin added

--- notification/src/main/java/com/kodama/notification/controller/NotificationController.java
+++ notification/src/main/java/com/kodama/notification/controller/NotificationController.java
@@ -19,6 +19,8 @@ public class NotificationController {
 
     @PostMapping
     public void sendNotification(@RequestBody NotificationRequest notificationRequest) {
+        log.info("+sendNotification(): notificationRequest: {}", notificationRequest);
         notificationService.send(notificationRequest);
+        log.info("-sendNotification()");
     }
 }
